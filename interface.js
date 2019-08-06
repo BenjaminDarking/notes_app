82214f3a782d9661a29dbaeba0bcf10603bf8006
Ben D <benjamin.d928@icloud.com>
2019-08-06 12:15:56 +0100

    Listing one item

--- interface.js
+++ interface.js
@@ -1,15 +1,29 @@
 //include document.ready equivalent in js
 
 
+document.addEventListener("DOMContentLoaded", function(event) {
+  var list = new List()
 
 
-// document.getElementById("create").addEventListener('click', function(){
-//   var note = new Note("testing the note");
-//   note.abbreviate();
-// });
+  document.getElementById("create").addEventListener('click', function(e) {
+    e.preventDefault();
+    text = document.getElementById("textBox").value;
+    list.addNote(new Note(text))
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(list.showNotes()[0].printNote()));
+    ul.appendChild(li);
+  });
+  //
+  // document.getElementById("textBox").change(function() {
+  //   text = document.getElementById("textBox").val();
+  // })
 
-// var ul = document.getElementById("notes");
-// var items = ul.getElementByTagName("li");
-// for (var i = 0; i < items.length; ++ i ) {
-//   return note.abbreviate();
-// }
+
+  // 
+  // var ul = document.getElementById("notes");
+  // var items = ul.getElementByTagName("li");
+  // for (var i = 0; i < items.length; ++i) {
+  //   return note.abbreviate();
+  // }
+});
