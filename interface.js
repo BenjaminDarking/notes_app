a27984d5c3826d20fe3c258403df52fcd7fa95b8
gennyallcroft <gennyallcroft@hotmail.co.uk>
2019-08-06 11:18:38 +0100

    add interface

---
+++ interface.js
@@ -0,0 +1,15 @@
+//include document.ready equivalent in js
+
+
+
+
+// document.getElementById("create").addEventListener('click', function(){
+//   var note = new Note("testing the note");
+//   note.abbreviate();
+// });
+
+// var ul = document.getElementById("notes");
+// var items = ul.getElementByTagName("li");
+// for (var i = 0; i < items.length; ++ i ) {
+//   return note.abbreviate();
+// }
