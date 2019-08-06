5edf66581562dd5b05e062fe1c0c5718aebf9802
TBrockster <tobybrock93@gmail.com>
2019-08-06 14:51:18 +0100

    note abbreviations have links

--- interface.js
+++ interface.js
@@ -8,12 +8,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById("create").addEventListener('click', function(e) {
     e.preventDefault();
     text = document.getElementById("textBox").value;
-    list.addNote(new Note(text))
+    list.addNote(new Note(text));
     var ul = document.getElementById("list");
-    var li = document.createElement("li");
+    var li = document.createElement("a");
+    li.href = '#' + list.showNotes()[list.showNotes().length - 1].abbreviate();
     li.appendChild(document.createTextNode(list.showNotes()[list.showNotes().length - 1].abbreviate()));
     ul.appendChild(li);
   });
+
+
+  function changeUrlForNote() {
+    window.addEventListener('hashchange', showNoteForCurrentPage);
+  };
+
+  function showNoteForCurrentPage() {
+    showNote(getNoteFromUrl(window.location));
+  };
+
+  function getNoteFromUrl(location) {
+    return location.hash.split("#")[1];
+  };
+
+  function showNote(note) {
+    document.getElementById('note').innerHTML = note;
+  };
   //
   // document.getElementById("textBox").change(function() {
   //   text = document.getElementById("textBox").val();
