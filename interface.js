91da06b2174bc9a7d21858c99eb7bff0743014c2
Rianne McCartney <riannemccartney@hotmail.com>
2019-08-06 16:40:05 +0100

    Further experimental additions to interface, still not working

--- interface.js
+++ interface.js
@@ -3,6 +3,7 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
   var list = new List()
+  var counter = 0
 
 
   document.getElementById("create").addEventListener('click', function(e) {
@@ -11,45 +12,38 @@ document.addEventListener("DOMContentLoaded", function(event) {
     list.addNote(new Note(text));
     var ul = document.getElementById("list");
     var li = document.createElement("a");
-    li.innerHTML = "<li id ='0'></li>"
+    li.innerHTML = "<li></li>"
+    li.setAttribute("id", counter)
+    counter += 1
 
     li.href = '#' + list.showNotes()[list.showNotes().length - 1].abbreviate();
     li.appendChild(document.createTextNode(list.showNotes()[list.showNotes().length - 1].abbreviate()));
     ul.appendChild(li);
-    console.log(list)
   });
 
-  document.getElementById('0').addEventListener('click', function(e) {
-    e.preventDefault();
-    changeUrlForNote();
-  });
-
+  // changeUrlForNote();
+  for (var i = 0; i <= counter; i++) {
+    document.getElementById(i).addEventListener('click', function(e) {
+      e.preventDefault();
+      showNoteForCurrentPage(i);
+      });
+  }
 
   function changeUrlForNote() {
     window.addEventListener('hashchange', showNoteForCurrentPage);
   };
 
-  function showNoteForCurrentPage() {
-    showNote(getNoteFromUrl(window.location));
+  function showNoteForCurrentPage(i) {
+    showNote(list.showNotes()[Number(document.getElementById(i))].printNote())
+
+    // showNote(getNoteFromUrl(window.location));
   };
 
-  function getNoteFromUrl(location) {
-    return location.hash.split("#")[1];
-  };
+  // function getNoteFromUrl(location) {
+  //   return location.hash.split("#")[1];
+  // };
 
   function showNote(note) {
-    document.getElementById('0').innerHTML = note;
+    document.getElementById('note').innerHTML = note;
   };
-  //
-  // document.getElementById("textBox").change(function() {
-  //   text = document.getElementById("textBox").val();
-  // })
-
-
-  //
-  // var ul = document.getElementById("notes");
-  // var items = ul.getElementByTagName("li");
-  // for (var i = 0; i < items.length; ++i) {
-  //   return note.abbreviate();
-  // }
 });
